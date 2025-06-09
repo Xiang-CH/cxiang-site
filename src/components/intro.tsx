93d72f2e32f70acd77f827c3131e47be8c84cb39
Xiang <cxiang@connect.hku.hk>
2025-06-09 11:05:05 +0800

    Update

---
+++ src/components/intro.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Intro() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col items-start justify-center">
+                <h1 className="text-2xl font-bold">Chen Xiang 陈想</h1>
+                <p className="text-lg">Software Engineer</p>
+            </div>
+            <div className="flex flex-col items-start justify-center">
+
+            </div>
+        </div>
+    )
+}
